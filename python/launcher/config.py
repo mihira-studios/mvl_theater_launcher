864f66124bc679b3ed13dc5969c829da9caa7d6e
Deepak Thapliyal <deepak.t@mihira.studio>
2025-12-30 15:28:15 +0530

    bugfix

--- python/launcher/config.py
+++ python/launcher/config.py
@@ -3,12 +3,12 @@ import os
 
 DOMAIN = os.environ.get("MVL_DOMAIN") if os.environ.get("MVL_DOMAIN") else "http://localhost"
 
-KC_BASE = os.getenv("KC_BASE", "{DOMAIN}:8080")
-KC_ISSUER = os.getenv("KC_BASE", "{DOMAIN}:8080")
+KC_BASE = os.getenv("KC_BASE", f"{DOMAIN}:8080")
+KC_ISSUER = os.getenv("KC_BASE", f"{DOMAIN}:8080")
 
 KC_REALM = os.getenv("KC_REALM","MIHIRA-REALM")
 KC_CLIENT_ID = os.getenv("KC_CLIENT_ID","mihira-cli")
 
-FASTAPI_BASE_URL = "{DOMAIN}:4007/api/v1"   
+FASTAPI_BASE_URL = f"{DOMAIN}:4007/api/v1"   
 FASTAPI_AUTH_PREFIX = ""     
 
